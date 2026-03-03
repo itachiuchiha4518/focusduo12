015b35f1655f7916482a8d1218dca3ab989e317a
itachiuchiha4518 <itachiuchiha184510@gmail.com>
2026-03-03 22:35:10 +0530

    Create firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,44 @@
+// lib/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut as firebaseSignOut } from "firebase/auth";
+import {
+  getFirestore, doc, getDoc, setDoc, collection, addDoc, query, where, getDocs, deleteDoc, updateDoc, onSnapshot
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8tzYEqejZMYvbapaLBOFHIS-wW6FIZPI",
+  authDomain: "focusduo-10ae1.firebaseapp.com",
+  projectId: "focusduo-10ae1",
+  storageBucket: "focusduo-10ae1.firebasestorage.app",
+  messagingSenderId: "611044167246",
+  appId: "1:611044167246:web:6b7391b00ed52aa3e7bd1f",
+  measurementId: "G-F6TH9RCYXC"
+};
+
+const app = initializeApp(firebaseConfig);
+try { getAnalytics(app); } catch(e) { /* ignore SSR analytics errors */ }
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export {
+  app,
+  auth,
+  provider,
+  signInWithPopup,
+  firebaseSignOut,
+  db,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  deleteDoc,
+  updateDoc,
+  onSnapshot
+};
